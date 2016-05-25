4b335fc9a844bc8ec6384eb9f67a540da7a9f642
Joshua May <notjosh@gmail.com>
2016-05-25 22:29:23 +1000

    Initial Commit

---
+++ SecretFlying/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SecretFlying
+//
+//  Created by joshua may on 25/05/2016.
+//  Copyright © 2016 joshua may. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SecretFlying/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SecretFlying
+//
+//  Created by joshua may on 25/05/2016.
+//  Copyright © 2016 joshua may. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
